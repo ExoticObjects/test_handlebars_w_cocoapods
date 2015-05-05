61b84ce536891b9f538192cbda8d08a1bd418455
Exotic Objects <jim@exoticobjects.com>
2015-05-05 11:27:04 -0400

    .

--- test_handlebars_w_cocoapods/EOAppDelegate.m
+++ test_handlebars_w_cocoapods/EOAppDelegate.m
@@ -1,9 +1,5 @@
 //
 //  EOAppDelegate.m
-//  test_handlebars_w_cocoapods
-//
-//  Created by Jim Kremens on 8/14/14.
-//  Copyright (c) 2014 Exotic Objects. All rights reserved.
 //
 
 #import "EOAppDelegate.h"
